6c65e1ab960706748f9848c59a2ac6b7d0f9de49
Sabari Santhosh Pillai <sabari.211096@gmail.com>
2025-07-07 14:44:04 +0530

    Comment out basePath in next.config.js for potential future use

--- client/next.config.js
+++ client/next.config.js
@@ -33,7 +33,7 @@ const aliasConfig = rootDirs.reduce((acc, dir) => {
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath:'/funnel-management',
+  // basePath:'/funnel-management',
   webpack: (config, { isServer }) => {
     // Ensure `config.externals` exists before modifying
     if (!isServer) {
